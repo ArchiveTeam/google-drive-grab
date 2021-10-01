c8b4d6db964381441337607c01654df164032ba5
arkiver <arkiver@protonmail.com>
2021-10-01 02:43:15 +0200

    Version 20211001.01. Use GNU Wget 1.20.3-at.20211001.01.

--- pipeline.py
+++ pipeline.py
@@ -40,7 +40,7 @@ if StrictVersion(seesaw.__version__) < StrictVersion('0.8.5'):
 WGET_AT = find_executable(
     'Wget+AT',
     [
-        'GNU Wget 1.20.3-at.20210504.01'
+        'GNU Wget 1.20.3-at.20211001.01'
     ],
     [
         './wget-at',
@@ -57,7 +57,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20210921.01'
+VERSION = '20211001.01'
 USER_AGENT = 'Archiveteam (https://wiki.archiveteam.org/; communicate at https://webirc.hackint.org/#ircs://irc.hackint.org/#archiveteam)'
 TRACKER_ID = 'google-drive'
 TRACKER_HOST = 'legacy-api.arpa.li'
